f59937d805157f9a843356e2c74d12bf2036dc20
godspower2020 <augustinegodspower40@gmail.com>
2023-09-22 05:30:41 +0100

    analytics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -65,7 +65,7 @@ const Footer = () => {
                           <span>sending...</span>
                         </div> :
                         <>
-                          Send message
+                          Send message 
                         </>               
                         }
                       </button>
